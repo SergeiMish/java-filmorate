856c133da4b9b4b82fffc419e7d2e08f97474241
SergeiMish <mishalevskii@yandex.ru>
2024-11-12 21:20:52 +0500

    ФЗ 10

--- java-filmorate-main/src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ java-filmorate-main/src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,13 +1,28 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
 
+    private long id;
+    @NotBlank
+    private String name;
+    @Length(max = 200)
+    private String description;
+    @NotNull
+    @PastOrPresent
+    private LocalDate releaseDate;
+    @Positive
+    private int duration;
 }
