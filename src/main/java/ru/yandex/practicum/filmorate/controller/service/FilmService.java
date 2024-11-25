ee722544d48118fd9d16ecf1af448e08de240bb2
Sergei Mishalevskii <108402627+SergeiMish@users.noreply.github.com>
2024-11-25 19:27:33 +0500

    ФЗ 11

--- src/main/java/ru/yandex/practicum/filmorate/controller/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/service/FilmService.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.filmorate.controller.service;
 
+import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.List;
 
+@Service
 public class FilmService {
 
     public Film addLike (Film film){
--- src/main/java/ru/yandex/practicum/filmorate/controller/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/service/UserService.java
@@ -1,10 +1,12 @@
 package ru.yandex.practicum.filmorate.controller.service;
 
+import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.interfaces.UserStorage;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
 
+@Service
 public class UserService {
 
     public User addFriend(User user){
