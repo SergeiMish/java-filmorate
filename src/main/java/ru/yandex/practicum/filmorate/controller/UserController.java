9b7b8fd24d59a740a0528d4646cb80c1e937e9b2
Aleksey Kolobov <www.kolabs@gmail.com>
2025-01-20 18:32:53 +0300

    Функциональность «Рекомендации».

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.dto.UserDto;
 import ru.yandex.practicum.filmorate.dto.mapper.UserDtoMapper;
 import ru.yandex.practicum.filmorate.interfaces.UserStorage;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.RecommendationService;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -24,6 +27,7 @@ public class UserController {
 
     private final UserStorage userStorage;
     private final UserService userService;
+    private final RecommendationService recommendationService;
 
     @PostMapping
     public UserDto postUser(@RequestBody @Valid UserDto userDto) {
@@ -35,8 +39,8 @@ public class UserController {
     @GetMapping
     public Collection<UserDto> getUsers() {
         return userStorage.getAll().stream()
-                .map(UserDtoMapper::toDto)
-                .collect(Collectors.toList());
+                          .map(UserDtoMapper::toDto)
+                          .collect(Collectors.toList());
     }
 
     @DeleteMapping("/{id}")
@@ -54,15 +58,15 @@ public class UserController {
     @GetMapping("/{id}/friends/common/{otherId}")
     public Set<UserDto> getCommonFriends(@PathVariable Long id, @PathVariable Long otherId) {
         return userService.getCommonFriends(id, otherId).stream()
-                .map(UserDtoMapper::toDto)
-                .collect(Collectors.toSet());
+                          .map(UserDtoMapper::toDto)
+                          .collect(Collectors.toSet());
     }
 
     @GetMapping("/{id}/friends")
     public Set<UserDto> getFriends(@PathVariable Long id) {
         return userService.listFriends(id).stream()
-                .map(UserDtoMapper::toDto)
-                .collect(Collectors.toSet());
+                          .map(UserDtoMapper::toDto)
+                          .collect(Collectors.toSet());
     }
 
     @PutMapping("/{id}/friends/{friendId}")
@@ -82,5 +86,10 @@ public class UserController {
         User user = UserDtoMapper.toModel(userDto);
         User updatedUser = userStorage.update(user);
         return ResponseEntity.ok(UserDtoMapper.toDto(updatedUser));
+    }
+
+    @GetMapping("/{id}/recommendations")
+    public List<Film> getRecommendations(@PathVariable Long id) {
+        return recommendationService.getRecommendations(id);
     }
 }
